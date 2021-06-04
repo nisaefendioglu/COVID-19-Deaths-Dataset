f2fb04c759ea6a91bcc3a9b4424a4a82875741fa
tolgasavass <85369212+tolgasavass@users.noreply.github.com>
2021-06-04 22:43:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd 
 import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
